0b57a3ce9d6ade0ff3665830f7bd2258c931302c
Daniel Vidakovich <dvidakovich@gmail.com>
2023-02-14 10:42:09 -0800

    added auth usr profiles

--- routes/courses.js
+++ routes/courses.js
@@ -6,13 +6,15 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({mergeParams: true});
 
+const {protect, authorize} = require('../middleware/auth');
+
 router.route('/').get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description'
 }),getCourses);
 router.route('/:id').get(getCourse);
-router.route('/').post(addCourse);
-router.route('/:id').put(updateCourse);
-router.route('/:id').delete(deleteCourse);
+router.route('/').post(protect, authorize('publisher', 'admin'), addCourse);
+router.route('/:id').put(protect, authorize('publisher', 'admin'), updateCourse);
+router.route('/:id').delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
