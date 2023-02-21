b6389be707f22cea9144d11844e3f5d3ca978da5
Daniel Vidakovich <dvidakovich@gmail.com>
2023-02-21 15:40:41 -0800

    added login/out

---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+        user: 'dff5c60eb8c22b',
+        pass: 'e2ff0bf99b1ece'
+        }
+    });
+
+    const message = {
+        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+    };
+
+    const info = await transporter.sendMail(message);
+
+    console.log('Message sent: %s', info.messageId);
+};
+
+module.exports = sendEmail;
